a9eab38dccfd7ec3320367c5fea0cf0879fa5dac
Joe Yates <joe.g.yates@gmail.com>
2016-12-26 21:56:13 +0100

    Add Monitor module

---
+++ lib/exbandwidth/monitor.ex
@@ -0,0 +1,51 @@
+defmodule Exbandwidth.Monitor do
+  # http://oid-info.com/get/1.3.6.1.2.1.2.2.1
+  @ifentry_oid [
+    1, # iso
+    3, # identified-orgaization
+    6, # dod
+    1, # internet
+    2, # mgmt
+    1, # mib-2 - http://www.ietf.org/rfc/rfc1213.txt
+    2, # interface
+    2, # ifTable
+    1  # ifEntry
+  ]
+  @if_octets_oids [in: 10, out: 16]
+
+  def start_link({snmpm}, if_index, direction) do
+    state = {snmpm, if_index, direction, nil}
+    pid = spawn_link(fn -> loop(state) end)
+    {:ok, pid}
+  end
+
+  defp loop({snmpm, if_index, direction, previous}) do
+    counter_oid = @if_octets_oids[direction]
+    :timer.sleep(1000)
+
+    call(snmpm, counter_oid, if_index)
+    |> extract_result
+    |> handle_result({snmpm, if_index, direction, previous})
+  end
+
+  defp call(snmpm, counter_oid, if_index) do
+    oids = @ifentry_oid ++ [counter_oid, if_index]
+    snmpm.sync_get('default_user', 'default_agent', [oids])
+  end
+
+  defp extract_result({:ok, answer, _}) do
+    {_, _, [{_, _, _, result, _}]} = answer
+    result
+  end
+
+  defp handle_result(result, {snmpm, if_index, direction, nil}) do
+    loop({snmpm, if_index, direction, result})
+  end
+  defp handle_result(result, {snmpm, if_index, direction, previous}) do
+    change = result - previous
+    receiver_pid = Process.whereis(:receiver)
+    GenServer.cast receiver_pid, {:traffic, direction, change}
+
+    loop({snmpm, if_index, direction, result})
+  end
+end
---
+++ test/exbandwidth/monitor_test.exs
@@ -0,0 +1,61 @@
+defmodule FakeSnmpm do
+  use GenServer
+
+  def start_link(replies) do
+    GenServer.start_link(__MODULE__, replies, name: :fake_snmpm)
+  end
+
+  def sync_get(_user, _agent, _oid_lists) do
+    pid = Process.whereis(:fake_snmpm)
+    GenServer.call pid, {:get}
+  end
+
+  def handle_call({:get}, from, [reply | rest]) do
+    GenServer.reply from, {:ok, {1, 2, [{3, 4, 5, reply, 6}]}, 7}
+    {:noreply, rest}
+  end
+end
+
+defmodule FakeReceiver do
+  use GenServer
+
+  def start_link(test_pid) do
+    GenServer.start_link(__MODULE__, test_pid, name: :receiver)
+  end
+
+  def handle_cast({:traffic, direction, bytes}, test_pid) do
+    send test_pid, {__MODULE__, :receive, {:traffic, direction, bytes}}
+    {:noreply, test_pid}
+  end
+end
+
+defmodule Exbandwidth.MonitorTestData do
+  def if_index, do: 12
+  def direction, do: :out
+end
+
+defmodule Exbandwidth.MonitorTest do
+  use ExUnit.Case, async: true
+  import Exbandwidth.MonitorTestData
+
+  setup context do
+    {:ok, _snmpm_pid} = FakeSnmpm.start_link(context[:replies])
+    {:ok, _receiver_pid} = FakeReceiver.start_link(self())
+
+    {:ok, pid} =
+      Exbandwidth.Monitor.start_link({FakeSnmpm}, if_index(), direction())
+
+    on_exit fn -> Process.exit(pid, :normal) end
+
+    [monitor_pid: pid]
+  end
+
+  @tag replies: [100, 500]
+  test "when the interface exists, it sends the traffic to the receiver" do
+    assert_receive {
+      FakeReceiver, :receive, {:traffic, direction, bytes}
+    }, 3000
+    assert direction == Exbandwidth.MonitorTestData.direction
+    assert bytes == (500 - 100)
+  end
+end
